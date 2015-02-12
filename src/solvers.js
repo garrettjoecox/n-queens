ca59105e7db2f4efb2e40c0ac52c6baac9ae5eb1
garrettjoecox <garrettjcox@gmail.com>
2015-02-12 11:32:49 -0800

    Completed sprint!

--- src/solvers.js
+++ src/solvers.js
@@ -12,7 +12,6 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-                                    //4
 window.findNRooksSolution = function(n) {
   var solution = [];
   var board = new Board({n:n});
@@ -26,50 +25,101 @@ window.findNRooksSolution = function(n) {
   return solution;
 };
 
-
-
-
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = 1; //fixmevar solutionCount = 1;
     for (var i = 1; i <= n; i++) {
        solutionCount *= i;
     }
-  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
-
-
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var board = new Board({n:n})
-
-  var recurse = function(currentCol){
-    var row = 0
-    while (row < n){
-      board.togglePiece(row, currentCol);
-      if (board.hasAnyQueensConflicts()) {
-        board.togglePiece(row, currentCol);
+  var board = new Board({n:n});
+  var solution = [];
+  var recurse = function(col) {
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(i, col);
+      if (!board.hasAnyQueensConflicts()) {
+        if (col === n-1) {
+          for (var j = 0; j < n;j++){
+            solution.push(board.get(j))
+          }
+        }
+        if (col < n-1) {
+          recurse(col+1);
+        }
       }
-      if (currentCol < n){
-        recurse(currentCol + 1);
-      };
-      row++;
-    };
-  };
+      if (solution.length > 0){
+        return;
+      }else{
+        board.togglePiece(i, col);
+      }
+    }
+  }
   recurse(0);
+  if (!solution.length) return board.rows();
+  return solution;
 
-  var results = [];
-  _.each(board.rows(), function(item){
-    results.push(item);
-  });
-  console.log(results);
-  // var solution = undefined; //fixme
-
-  // console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return results;
 };
+
+
+// return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
+window.countNQueensSolutions = function(n) {
+  if (n <= 1) return 1;
+  var board = new Board({n:n});
+  var count = 0;
+  var recurse = function(col) {
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(i, col);
+      if (!board.hasAnyQueensConflicts()) {
+        if (col === n-1) {
+          count++;
+        }
+        if (col < n) {
+          recurse(col+1);
+        }
+      }
+     board.togglePiece(i, col);
+    }
+  }
+  recurse(0);
+  return count;
+};
+
+
+
+
+
+
+//   var board = new Board({n:n})
+
+//   var recurse = function(currentCol){
+//     var row = 0
+//     while (row < n){
+//       board.togglePiece(row, currentCol);
+//       if (board.hasAnyQueensConflicts()) {
+//         board.togglePiece(row, currentCol);
+//       }
+//       if (currentCol < n){
+//         recurse(currentCol + 1);
+//       };
+//       row++;
+//     };
+//   };
+//   recurse(0);
+
+//   var results = [];
+//   _.each(board.rows(), function(item){
+//     results.push(item);
+//   });
+//   console.log(results);
+//   var solution = undefined; //fixme
+
+//   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+//   return results;
+// };
 
 // findNRooksSolutions(n){
 //   var board = new Board({n:n});
@@ -86,18 +136,3 @@ window.findNQueensSolution = function(n) {
 //     }
 //   }
 //   recurse(col);
-
-
-
-
-
-
-// return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n) {
-  //temp but works for now
-  var haha = [1, 1, 0, 0, 2, 10, 4, 40, 92];
-
-
-  // console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return haha[n];
-};
